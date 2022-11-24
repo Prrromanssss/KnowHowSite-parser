19c63f3f08c29be4bb7b730a275f7804e7c0b637
Sourr_cream <roman_boyko.48@mail.ru>
2022-11-25 00:50:33 +0300

    change parser and lint code

--- main.py
+++ main.py
@@ -1,47 +1,52 @@
-import requests
-from bs4 import BeautifulSoup
+import csv
 import os
 import shutil
-import csv
-from time import sleep
 from random import randrange
-# Для начала работы нужно установить 3 модуля
-# beautifulsoup4 для превращения html страницы к объекту beautifulsoup и дальнейшей работы с ней
-# (взятие тегов, элементов и тд)
-# requests для получения html страниц сайта
-# lxml подойдет лучше всего для парса html страницы, используется как второй позиционный аргумент
-# передаваемый классу Beautifulsoup
+from time import sleep
+
+import requests
+from bs4 import BeautifulSoup
 
 
 def create_first_page_obj_each_folder():
-    """Создать первую страницу для каждой папки
+    """Create the first page for each folder
 
-    Функция получает html-код первой страницы объектов для каждой категории товаров(headers) и записывает в файл
+        The function receives the html code of the first page of objects
+        for each product category (headers) and writes it to a file
     """
     for key, value in all_content.items():
         print(key)
         for href in value:
-            if ((href['href'].count('/') == 3 and key in ['Смартфоны', 'Планшеты', 'Ноутбуки']
+            if ((href['href'].count('/') == 3 and key in [
+                'Смартфоны',
+                'Планшеты',
+                'Ноутбуки'
+                ]
                  and 'naushniki' not in href['href'])
-                    or (key in ['Гаджеты', 'Аудио'] and ('gadzhety' in href['href']
-                                                         or href['href'][
-                                                            href['href'].rindex('/') + 1:] == 'naushniki'))):
+                    or (key in ['Гаджеты', 'Аудио'] and (
+                        'gadzhety' in href['href']
+                        or href['href'][
+                                        href['href'].rindex('/') + 1:] ==
+                        'naushniki'))):
 
                 if href.find("img")["alt"] == 'Оргтехника':
                     continue
-                get_and_write(file=f'headers/{key}/list_objects_{href.find("img")["alt"]}.html',
-                              href=f'{url}/{href["href"]}')
+                get_and_write(
+                    file=f'headers/{key}/list_objects_'
+                    f'{href.find("img")["alt"]}.html',
+                    href=f'{url}/{href["href"]}')
                 print(f'{url}/{href["href"]}')
         print()
         sleep(randrange(2, 5))
 
 
 def safe_print(*args, **kwargs):
-    """Безопасный принт с возвращением результата
+    """Safe print with result return
 
-       Принт подавляет исключения, если они возникли и возвращает значения аргументов, если они есть,
-       иначе '-'
-        """
+        Print suppresses exceptions if they occur and returns the argument
+        values if any,
+        otherwise '-'
+    """
     try:
         print(*args, **kwargs)
         rez = args[0]
@@ -52,10 +57,11 @@ def safe_print(*args, **kwargs):
 
 
 def get_and_write(file, href):
-    """Получить html страницу и записать ее в файл
+    """Get html page and write it to file
 
-    Функция получает html-код страницы и записывает все содержимое в файл.
-    Это делается для того, чтобы не перегружать сайт get-запросами
+        The function receives the html code of the page and writes all
+        the content to a file.
+        This is done in order not to overload the site with get requests.
     """
     with open(file, 'w') as html_file:
         req = requests.get(href, headers=headers_for_req)
@@ -64,10 +70,11 @@ def get_and_write(file, href):
 
 
 def take_hrefs_of_objects(headers, html):
-    """Получить все ссылки со страницы
+    """Get all links from a page
 
-    Функция открывает html файл, делает его объектом BeautifulSoup и находит там все ссылки,
-    записывая их по соответствующему ключу, которому принадлежит html-страница
+        The function opens the html file, makes it a BeautifulSoup object and
+        finds all the links there,
+        writing them by the corresponding key to which the html page belongs
     """
     for folder in headers:
         with open(f'headers/{folder}/{html}.html') as file:
@@ -78,33 +85,38 @@ def take_hrefs_of_objects(headers, html):
 
 
 def create_html_each_folder():
-    """Создать html файл для каждой категории из headers
+    """Create html file for each category from headers
 
-        Функция создает папку на каждую категорию из headers.
-        Получает html-код начальной страницы каждой категории(headers) и загружает его в файл
+        The function creates a folder for each category from headers.
+        Gets the html code of the home page of each category (headers) and
+        loads it into a file
     """
     for item in all_categories:
         try:
             os.mkdir(f'headers/{item.text}')
         except FileExistsError:
             pass
-        get_and_write(file=f'headers/{item.text}/index.html', href=f'{url}/{item["href"]}')
+        get_and_write(
+            file=f'headers/{item.text}/index.html',
+            href=f'{url}/{item["href"]}'
+        )
         sleep(randrange(2, 5))
         print(f'{item.text}: {url}/{item["href"]}')
 
 
 def load_all_pages(folder, html, src, namefile, count=1):
-    """Загрузить все страницы объектов для каждой категории
+    """Download all object pages for each category
 
-        Функция рекурсивно обходит все страницы объектов, которые принадлежат соответствующим категориям(headers)
-        и записывает их в файл
+        The function recursively traverses all pages of objects
+        that belong to the corresponding categories (headers)
+        and writes them to a file
     """
     with open(f'headers/{folder}/{html}', 'w') as file:
         file.write(src)
         soup = BeautifulSoup(src, 'lxml')
         try:
             print(soup.find('ul', class_='pager').find_all('li')[-1])
-        except AttributeError as e:  # Значит это страница единственная
+        except AttributeError:  # It means that this page is single
             return
         soup = str(soup.find('ul', class_='pager').find_all('li')[-1])
         if 'следующая' in soup:
@@ -119,25 +131,29 @@ def load_all_pages(folder, html, src, namefile, count=1):
 
 
 def create_first_page_obj_audio():
-    """
-        Создать первую страницу для папки аудио
+    """Create first page for audio folder
 
-        Функция получает html-код первой страницы объектов для папки аудио и записывает в файл.
-        Так как структура страницы, где находятся объекты аудио немного отличается,
-         нам приходится погружаться еще глубже, чтобы достать первую страницу объектов аудио
-        """
+        The function gets the html code of the first page of objects
+        for the audio folder and writes it to a file.
+        Since the structure of the page where the audio objects are located
+        is slightly different,
+        we have to dive even deeper to get the first page of audio objects
+    """
     for href in all_content['Аудио']:
         if '-naushniki-' in href["href"][href["href"].rindex('/'):]:
-            get_and_write(file=f'headers/Аудио/list_objects_{href.find("img")["alt"]}.html',
-                          href=f'{url}/{href["href"]}')
+            get_and_write(
+                file=f'headers/Аудио/list_objects_'
+                f'{href.find("img")["alt"]}.html',
+                href=f'{url}/{href["href"]}')
 
             print(f'{url}/{href["href"]}')
 
 
 def load_all_pages_in_folders():
-    """Загрузить все страницы для каждой категории
+    """Download all pages for each category
 
-        Функция проходит по всем начальным страницам каждой категории и рекурсивно загружает оставшиеся соответствующие
+        The function iterates through all the start pages of each category and
+        recursively loads the remaining matching ones.
 
     """
     for folder in headers:
@@ -148,16 +164,25 @@ def load_all_pages_in_folders():
                     continue
                 with open(f'headers/{folder}/{file}') as rfile:
                     src = rfile.read()
-                    name_file = rfile.name[rfile.name.rindex('/') + 1:rfile.name.rindex('.')]
+                    name_file = (
+                        rfile.name[rfile.name.rindex('/') + 1:
+                                   rfile.name.rindex('.')]
+                        )
                     os.remove(f'headers/{folder}/{file}')
-                    load_all_pages(folder, f'{name_file}_page1.html', src, name_file)
+                    load_all_pages(
+                        folder,
+                        f'{name_file}_page1.html',
+                        src,
+                        name_file
+                        )
                     print(f'{name_file.upper()}: LOADED')
 
 
 def write_in_csv():
-    """Получить данные и записать в csv файл
+    """Get data and write to csv file
 
-    Функция заходит на страницу каждого объекта, берет необходимые данные и записывает их в соответствующий csv файл
+        The function visits the page of each object, takes the necessary data
+        and writes it to the corresponding csv file
     """
 
     for folder in headers:
@@ -165,8 +190,12 @@ def write_in_csv():
         walks = os.walk(f'headers/{folder}')
         for files in walks:
             with open(f'data/{folder}.csv', 'w') as file_csv:
-                writer = csv.DictWriter(file_csv, fieldnames=fieldnames, delimiter=';',
-                                        quoting=csv.QUOTE_NONNUMERIC)
+                writer = csv.DictWriter(
+                    file_csv,
+                    fieldnames=fieldnames,
+                    delimiter=';',
+                    quoting=csv.QUOTE_NONNUMERIC
+                    )
                 writer.writeheader()
                 for file in files[-1]:
                     print(file)
@@ -174,10 +203,16 @@ def write_in_csv():
                         continue
                     with open(f'headers/{folder}/{file}') as rfile:
                         src = rfile.read()
-                    soup = BeautifulSoup(src, 'lxml').find('ul', class_='catalog_items').find_all('li')
+                    soup = BeautifulSoup(src, 'lxml').find(
+                        'ul',
+                        class_='catalog_items'
+                        ).find_all('li')
                     for li in soup:
-                        if not (any(name in li.text for name in names_obj[folder]) and "РУБ." in li.text
-                                and li.text != '.DS_Store'):
+                        if not (
+                            any(name in li.text for name in names_obj[folder])
+                            and "РУБ." in li.text
+                            and li.text != '.DS_Store'
+                                ):
                             continue
                         data = {}
                         href_of_page = f'{url}/{li.find("div", class_="catalog_item hoverable catalog_item_ops_bottom catalog_item_simplified").find("a", class_="catalog_item_link")["href"]}'
@@ -187,11 +222,19 @@ def write_in_csv():
                         print(href_of_page)
                         data['Категория'] = safe_print(f"{folder}")
                         data['Название'] = safe_print(f"{soup.find('h1').text}")
-                        if data['Название'] in ['Ноутбуки MSI', 'Все варианты Lenovo IdeaCentre 5 24IOB6',
-                                                'Моноблоки Lenovo', 'Мониторы Iiyama', 'Мониторы AOC']:
+                        if data['Название'] in [
+                            'Ноутбуки MSI',
+                            'Все варианты Lenovo IdeaCentre 5 24IOB6',
+                            'Моноблоки Lenovo',
+                            'Мониторы Iiyama',
+                            'Мониторы AOC'
+                        ]:
                             continue
                         try:
-                            href = soup.find('div', class_='catalog_brand_more').find('a')
+                            href = soup.find(
+                                'div',
+                                class_='catalog_brand_more'
+                                ).find('a')
                             data['Все товары от бренда'] = safe_print(f"{url}/{href['href']}")
                         except AttributeError:
                             data['Все товары от бренда'] = '-'
@@ -202,7 +245,10 @@ def write_in_csv():
                             data['Фото'] = '-'
                         data['Преимущество'] = safe_print(
                             f"{soup.find('div', class_='catalog_variant_specs').text}")
-                        href = soup.find('ul', class_='catalog_variant_addon_other').find('a')
+                        href = soup.find(
+                            'ul',
+                            class_='catalog_variant_addon_other'
+                            ).find('a')
                         try:
                             base_charac = \
                             f"{soup.find('div', class_='catalog_variant_addon_characteristics').text}".split(':')[
@@ -243,7 +289,12 @@ def write_in_csv():
                                 count += 1
                             if count == 4:
                                 break
-                        for category in ['Память', 'RAM', 'Частота и количество ядер процессора', 'ОС']:
+                        for category in [
+                            'Память',
+                            'RAM',
+                            'Частота и количество ядер процессора',
+                            'ОС'
+                        ]:
                             if category not in data:
                                 data[category] = '-'
                         data['Остальные характеристики'] = safe_print(f"{'•'.join(base_rest)}")
@@ -269,7 +320,7 @@ def main():
     get_and_write(file='index.html', href=url)
     with open('index.html') as file:
         src = file.read()
-    soup = BeautifulSoup(src, 'lxml')
+    soup = BeautifulSoup(src, 'html.parser')
     all_categories = soup.find('ul', class_='header_groups').find_all('a', class_='popup_handle header_group')
     all_categories = list(filter(lambda x: 'Apple' not in str(x) and 'Аксессуары' not in str(x), all_categories))
     create_html_each_folder()
@@ -290,44 +341,84 @@ def main():
         pass
     write_in_csv()
 
-    # os.remove('index.html')
-    # shutil.rmtree('headers')  # Удалить папку с html страницами сайта
+    # Delete folder with html pages of the site
+    os.remove('index.html')
+    shutil.rmtree('headers')
 
 
+# Creating a root directory where all html files will be stored
 try:
-    os.mkdir('headers')  # Создание корневого каталога, где будут храниться все html файлы
+    os.mkdir('headers')
 except FileExistsError:
     pass
 
-url = 'https://xn----8sb1bezcm.xn--p1ai/'  # url главной страницы сайта
+# home page url
+url = 'https://xn----8sb1bezcm.xn--p1ai/'
 
+# This is necessary so that the site does not think that
+# we are a bot and give us their html pages
 headers_for_req = {
-    'accept': '*/*',  # Это необходимо, чтобы сайт не подумал, что мы бот и дал нам свои html страницы
-    'User-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7)' 
-                  ' AppleWebKit/537.36 (KHTML, like Gecko) Chrome/102.0.0.0 Safari/537.36'
+    'accept': '*/*',
+    'User-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7)'
+                  ' AppleWebKit/537.36 (KHTML, like Gecko) '
+                  'Chrome/102.0.0.0 Safari/537.36'
 }
 
+# Relevant categories == future datasheets
 headers = (
     'Смартфоны',
     'Планшеты',
-    'Ноутбуки',  # Соответствующие категории == будущие таблицы данных
+    'Ноутбуки',
     'Гаджеты',
     'Аудио'
 )
 
+# Subcategories of each category
 names_obj = {
-    'Смартфоны': ['Смартфон'],
-    'Планшеты': ['Планшет'],
-    'Ноутбуки': ['Ноутбук', 'Монитор', 'Моноблок'],  # Подкатегории каждой категории
-    'Гаджеты': ['Смарт-браслеты', 'Смарт-часы'],
-    'Аудио': ['Наушники', 'наушники', 'Гарнитура'],
+    'Смартфоны': [
+        'Смартфон'
+        ],
+    'Планшеты': [
+        'Планшет'
+        ],
+    'Ноутбуки': [
+        'Ноутбук',
+        'Монитор',
+        'Моноблок'
+        ],
+    'Гаджеты': [
+        'Смарт-браслеты',
+        'Смарт-часы'
+        ],
+    'Аудио': [
+        'Наушники',
+        'наушники',
+        'Гарнитура'
+        ],
 }
 
-fieldnames = ['Категория', 'Название', 'Все товары от бренда', 'Фото', 'Преимущество', 'ОС',  # Поля в csv файлах
-              'Частота и количество ядер процессора', 'RAM', 'Память', 'Остальные характеристики',
-              'Все характеристики', 'Описание', 'Код товара', 'Цена', 'Баллы за покупку', 'Ссылка на товар']
+# Fields in csv files
+fieldnames = [
+    'Категория',
+    'Название',
+    'Все товары от бренда',        
+    'Фото',
+    'Преимущество',
+    'ОС',
+    'Частота и количество ядер процессора',
+    'RAM',
+    'Память',
+    'Остальные характеристики',
+    'Все характеристики',
+    'Описание',
+    'Код товара',
+    'Цена',
+    'Баллы за покупку',
+    'Ссылка на товар'
+    ]
 
 all_content = {folder: [] for folder in headers}
+
 
 if __name__ == '__main__':
     main()
